a08a4f3c387b0090691e1e1b045767446cc6a03e
magoyoo <magoyoooooo@gmail.com>
2024-04-09 23:24:25 +0900

    파비콘 변경 / 칼라시스템 등록

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,49 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      color: {
+        "grey-100": "#FAFBFC",
+        "grey-200": "#F5F7FA",
+        "grey-300": "#E2E5E9",
+        "grey-400": "#A8ADB4",
+        "grey-500": "#8B9199",
+        "grey-600": "#6E767E",
+        "grey-700": "#515A63",
+        "grey-800": "#343B42",
+        "grey-900": "#171E24",
+
+        "primary-100": "#E1E6EB",
+        "primary-200": "#B3C2CD",
+        "primary-300": "#849DB0",
+        "primary-400": "#5681A0",
+        "primary-500": "#296583",
+        "primary-600": "#1D4F6F",
+        "primary-700": "#123C5A",
+        "primary-800": "#0A2A46",
+        "primary-900": "#0E2844",
+
+        "yellow-300": "#F9F3D2",
+        "yellow-400": "#F5ECB8",
+        "yellow-500": "#F2E59D",
+        "yellow-600": "#EEDC82",
+        yellow: "#F2D024",
+        green: "#38A169",
+        red: "#E53E3E",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
