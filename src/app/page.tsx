66afc6db937ba8a9f837c5551cd6c1e2415ab0ea
daltodalto <dalssi.dalto@gmail.com>
2024-04-12 21:30:05 +0900

    add banner section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import MainHomeBanner from "@/components/banner/MainHomeBanner";
 import Header from "@/components/common/Header";
 import { PostSort } from "@/interfaces/post";
 import { getAllPostMeta, getAllPosts } from "@/lib/api";
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <>
       <Header />
+      <MainHomeBanner />
       <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
     </>
   );
---
+++ src/components/banner/MainHomeBanner.tsx
@@ -0,0 +1,16 @@
+const MainHomeBanner = () => {
+  return (
+    <section className="hidden lg:flex justify-center items-center h-[500px] mt-[80px] border-b-[1px] border-gray-900">
+      <div className="flex flex-col gap-[30px] xl:w-[1200px] w-full h-full pl-[40px] justify-center items-start">
+        <h1 className="text-gray-800 text-[36px] font-semibold">
+          새 롭 지 만 익 숙 하 게
+        </h1>
+        <h2 className="text-gray-700 font-medium text-[24px]">
+          이 모든 것이 나의 생과 함께하길 기대한다.
+        </h2>
+      </div>
+    </section>
+  );
+};
+
+export default MainHomeBanner;
