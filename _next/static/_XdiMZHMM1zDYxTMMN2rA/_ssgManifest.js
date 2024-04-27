ec919046a66076d894daa50ae061b724d1327036
daltodalto <dalssi.dalto@gmail.com>
2024-04-27 22:57:16 +0900

    temp comit for deploy

---
+++ _next/static/_XdiMZHMM1zDYxTMMN2rA/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fkr\u002Falgorithm\u002F[slug]","\u002Fkr\u002Fswift\u002F[slug]","\u002Fkr\u002Ftags\u002F[slug]","\u002Fkr\u002Fweb\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
